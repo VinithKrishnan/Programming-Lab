292dbeaed60cc4960552f39f0aeaa4ae3f738531
VinithKrishnan <vinithk@gmail.com>
2017-02-17 07:09:34 +0530

    Classes implemented

--- linkedstack.cpp
+++ linkedstack.cpp
@@ -6,44 +6,69 @@ struct node
 struct node* next;
 };
 
-void  push(struct node** head,char data)
-{struct node* newnode = new node();
-newnode->data=data;
-newnode->next=*head;
-*head=newnode;
 
+class Stack
+{public:
+	struct node* head;
+     Stack();
+     void push(char data);
+     void pop();
+     char top();
+};
+
+Stack::Stack()
+{head=NULL;
 }
 
-void pop(struct node** head)
-{ if (*head == NULL)
+void Stack::push(char data)
+{struct node* newnode = new node();
+newnode->data=data;
+newnode->next=head;
+head=newnode;
+}
+
+void Stack::pop()
+{ if (head == NULL)
       {cout<<"\n Paranthesis are imperfectly matched";
       exit(0);
 	  }
-struct node* temp= *head;
-*head=temp->next;
+struct node* temp= head;
+head=temp->next;
 delete temp;
 }
 
-
+char Stack::top()
+{if(head!=NULL)
+return head->data;
+else
+return '0';
+}
 
 
 
 
 int main()
 {
-
-struct node* head=NULL;
+Stack stack=Stack();
 string s;
+int flag=0;
 cout<<"\n Enter string of paranthesis";
 cin>>s;
 for(int i=0;i<s.size();i++)
 {if(s[i]=='{')
-push(&head,'{');
+stack.push('{');
 else if(s[i]=='}')
-pop(&head);
-}
-if(head==NULL)
-cout<<"\n Parenthesis match perfectly";
+{if(stack.top()=='{')
+stack.pop();
 else
+{
 cout<<"\n Paranthesis are imperfectly matched";
+flag=1;
+break;
+}
+}
+}
+if(stack.head==NULL && flag==0) 
+cout<<"\n Parenthesis match perfectly";
+
 }
